1bd441c68d8f1ba3038c59bb7f270155cf866b61
zhongdet <thomas980930@gmail.com>
2026-08-05 10:00:06 +0800

    fix load_game() type error of special station

--- flask/app/models/stations.py
+++ flask/app/models/stations.py
@@ -8,15 +8,15 @@ class Stations(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     
     name = db.Column(db.String(64), unique=True, nullable=False)
-    is_special = db.Column(db.String(64), nullable=False)
+    is_special = db.Column(db.Boolean, nullable=False)
     hidden = db.Column(db.Boolean, default=False)
     owner_team = db.Column(db.String(32), nullable=True)
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
     
-    def __init__(self, name: str, is_special: str, hidden: bool=False, owner_team: Optional[str]=None) -> None:
+    def __init__(self, name: str, is_special: bool, hidden: bool=False, owner_team: Optional[str]=None) -> None:
         self.name: str = name
         self.hidden: bool = hidden
-        self.is_special: str = is_special
+        self.is_special: bool = is_special
         self.owner_team: Optional[str] = owner_team
     
     def __repr__(self):
